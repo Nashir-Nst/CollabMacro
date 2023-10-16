2fa86b9be13915df70e207bd9212afee75fdeb54
Nashir-Nst <muhammadlnashir738@gmail.com>
2023-10-16 17:08:35 +0700

    udah ada scroll view tapi eror gradlenya

---
+++ app/src/main/java/com/nashir/project/DeskripsiActivity.kt
@@ -0,0 +1,11 @@
+package com.nashir.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeskripsiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deskripsi)
+    }
+}
